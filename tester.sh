42470025b8b653a26ee8629a307111c948d1e4bb
Lucas Jacquemin <lucas.jacquemin2002@gmail.com>
2024-03-18 09:55:21 +0100

    fix for sorted input

--- tester.sh
+++ tester.sh
@@ -39,70 +39,70 @@ fi
 echo "${GREEN} Yep! Normi is good :D${NORMAL}"
 echo ""
 
-# # Check if push_swap return Error whe invalid input
-# echo "${CYAN}Checking invalid input...${NORMAL}"
-# INVALID_INPUT=(
-# "" "a" " " "1 1"
-# "Hello, wolrd !"
-# "0 one 1 2 3"
-# "1 1 2 3 4 5 6 7 8 9 10"
-# "1 2 3 4 5 6 7 8 9 10 1"
-# "2 3 4 5 6 7 8 9 10 1 1"
-# "654684 168468 4512 324132 13546 2435464 168468"
-# "1 2 2147483647 3 4 5 6 7 8 9 2147483647"
-# "-2 -1 0 1 2 3 4 5 6 -1"
-# # "-2 -1 0 1 2 3 4 5 6"
-# "-2147483649 1 2 3"
-# "1 2 3 2147483648"
-# "1 2 3 123132131313"
-# "1 2 3 1231321313132"
-# "1 2 3 12313213131322"
-# "1 2 3 1231321313132256468798795")
-# LEN=${#INVALID_INPUT[@]}
-# EXPECTED="Error$"
-# SUCCESSNB=0
+# Check if push_swap return Error whe invalid input
+echo "${CYAN}Checking invalid input...${NORMAL}"
+INVALID_INPUT=(
+"" "a" " " "1 1"
+"Hello, wolrd !"
+"0 one 1 2 3"
+"1 1 2 3 4 5 6 7 8 9 10"
+"1 2 3 4 5 6 7 8 9 10 1"
+"2 3 4 5 6 7 8 9 10 1 1"
+"654684 168468 4512 324132 13546 2435464 168468"
+"1 2 2147483647 3 4 5 6 7 8 9 2147483647"
+"-2 -1 0 1 2 3 4 5 6 -1"
+# "-2 -1 0 1 2 3 4 5 6"
+"-2147483649 1 2 3"
+"1 2 3 2147483648"
+"1 2 3 123132131313"
+"1 2 3 1231321313132"
+"1 2 3 12313213131322"
+"1 2 3 1231321313132256468798795")
+LEN=${#INVALID_INPUT[@]}
+EXPECTED="Error$"
+SUCCESSNB=0
 
-# for ((i=0; i<$LEN; i++)); do
-# 	OUTPUT=$(./push_swap ${INVALID_INPUT[i]} | cat -e)
+for ((i=0; i<$LEN; i++)); do
+	OUTPUT=$(./push_swap ${INVALID_INPUT[i]} | cat -e)
 
-# 	# echo "testing input ${INVALID_INPUT[i]}, Output : \"${OUTPUT}\""
-# 	echo -ne "Test $i/$LEN: ${INVALID_INPUT[i]}                                   "\\r
-# 	sleep 0.1
-# 	if [ "$OUTPUT" != "$EXPECTED" ]; then
-# 		echo "push_swap does not return \"${EXPECTED}\" with invalid input ${INVALID_INPUT[i]}                                                                                             "
-# 	else
-# 		((SUCCESSNB++))
-# 	fi
-# done
+	# echo "testing input ${INVALID_INPUT[i]}, Output : \"${OUTPUT}\""
+	echo -ne "Test $i/$LEN: ${INVALID_INPUT[i]}                                   "\\r
+	sleep 0.1
+	if [ "$OUTPUT" != "$EXPECTED" ]; then
+		echo "push_swap does not return \"${EXPECTED}\" with invalid input ${INVALID_INPUT[i]}                                                                                             "
+	else
+		((SUCCESSNB++))
+	fi
+done
 
-# if [ "$SUCCESSNB" == "$LEN" ]; then
-# 	echo "${GREEN} Yep! $SUCCESSNB/$LEN Error handling is good :D                                              "
-# else
-# 	echo "${RED} NOO! $SUCCESSNB/$LEN                                             "
-# fi
-# echo ""
+if [ "$SUCCESSNB" == "$LEN" ]; then
+	echo "${GREEN} Yep! $SUCCESSNB/$LEN Error handling is good :D                                              "
+else
+	echo "${RED} NOO! $SUCCESSNB/$LEN                                             "
+fi
+echo ""
 
 
-# # Check if push_swap sorts correctly
-# echo "${CYAN}CHECKER Test, until size 300:${NORMAL}"
-# for ((i=1; i<301; i++)); do
-#     ARG=$(./nbr_gen $i)
-#     OUTPUT=$(./push_swap $ARG)
-#     OUT=$(echo "$OUTPUT" | tr " " "\n")
-#     LINES_COUNT=$(echo "$OUT" | wc -l)
-#     if [ "$OUT" == "" ]; then
-#     	RES="NO_INST"
-#     else
-#     	RES=$(echo "$OUT" | ./checker_linux $ARG)
-#     fi
-#     echo -ne "Array size $i: CHECKER $RES (Complexity: $LINES_COUNT)                 "\\r
-#     if [ "$RES" != "OK" ] && [ "$RES" != "NO_INST" ]; then
-#     	echo "./push_swap $ARG" > command_log
-#     	printError "CHECKER KO ! See command_log for command detail" "Final Note: 0"
-#     fi
-# done
-# echo "${GREEN} Yep! CHECKER is good :D                                              "
-# echo ""
+# Check if push_swap sorts correctly
+echo "${CYAN}CHECKER Test, until size 300:${NORMAL}"
+for ((i=1; i<301; i++)); do
+    ARG=$(./nbr_gen $i)
+    OUTPUT=$(./push_swap $ARG)
+    OUT=$(echo "$OUTPUT" | tr " " "\n")
+    LINES_COUNT=$(echo "$OUT" | wc -l)
+    if [ "$OUT" == "" ]; then
+    	RES="NO_INST"
+    else
+    	RES=$(echo "$OUT" | ./checker_linux $ARG)
+    fi
+    echo -ne "Array size $i: CHECKER $RES (Complexity: $LINES_COUNT)                 "\\r
+    if [ "$RES" != "OK" ] && [ "$RES" != "NO_INST" ]; then
+    	echo "./push_swap $ARG" > command_log
+    	printError "CHECKER KO ! See command_log for command detail" "Final Note: 0"
+    fi
+done
+echo "${GREEN} Yep! CHECKER is good :D                                              "
+echo ""
 
 
 # Check if push_swap sorts correctly and meets required performance
@@ -146,7 +146,6 @@ for ((i=0; i<$LEN; i++)); do
 	echo "${GREEN} Passed all $TESTNB tests ! Complexity Size=${SIZE[i]} was good :D               "
 	AV=$(echo "scale=2; $SUM / $TESTNB" | bc)
 	printf "${GREEN} Average = ${BOLD}%.2f${NORMAL}${GREEN} Max = ${BOLD}%d${NORMAL}\n" "$AV" "${MAX_LINES}"
-
 done
 
 rm push_swap
